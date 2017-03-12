e6b3d2ece9474ff7ca489fae60d2bc926c3d117b
Elijah <Bekwar@mail.ru>
2017-03-12 16:01:18 +0300

    4th lab

---
+++ 4/4.1.4/1.cpp
@@ -0,0 +1,85 @@
+﻿//4.1.(4). Дано n матриц.В каждой строке каждой матрицы записано одно
+//         слово.Если в слове нет повторяющихся символов – преобразовать
+//		   все буквы слова в прописные, в противном случае – в строчные
+//         буквы.Полученные матрицы вывести.
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+void output(char **a,int str)
+{
+	for (int i = 0; i < str; i++)
+	{
+		for (int j = 0; j != '\n'; j++)
+		{
+			if (a[i][j] < 65 || a[i][j] > 122) break;
+			else cout << a[i][j];
+		}
+		cout << endl;
+	}
+}
+void objective(char **a, int str)
+{
+	for (int i = 0; i < str; i++)               // поиск повторяющихся символов
+	{											// буквы отличающиеся регистром не являются повторяющимися
+		int count = 0;
+		for (int j = 0; j < 255; j++)
+		{
+			for (int k = j + 1; k < 255; k++)
+			{
+				if (a[i][j] < 65 || a[i][j] > 122) break;
+				if (a[i][j] == a[i][k]) count++;
+			}
+		}
+		for (int j = 0; j < 255; j++)           // замена 
+		{
+			if (count >= 1)
+			{
+				if (a[i][j] <= 122 && a[i][j] >= 97) continue;
+				else a[i][j] = a[i][j] + 32;
+			}
+			if (count == 0)
+			{
+				if (a[i][j] <= 90 && a[i][j] >= 65) continue;
+				else a[i][j] = a[i][j] - 32;
+			}
+		}
+		count = 0;
+	}
+}
+
+int main()
+{
+	int n = 0, str = 0;
+    cout << "Enter the number of matrices : ";
+	cin >> n;
+
+	for (int f = 0; f < n; f++)
+	{
+		system("cls");
+		cout << "Matrix " << f + 1 << ": " << endl;
+		cout << "Enter the number of lines in this matrix : ";
+		cin >> str;
+
+		cout << "Enter the lines: " << endl;
+		char **a = new char*[str];
+		for (int i = 0; i < str; i++)
+		{
+			a[i] = new char[255];
+			cin >> a[i];
+			cin.clear();
+		}
+
+		objective(a, str);
+
+		cout << endl << "The result:" << endl << endl;
+		output(a,str);
+
+		system("pause");
+
+	}
+	
+    return 0;
+}
+
---
+++ 4/4.2.4/2.cpp
@@ -0,0 +1,81 @@
+﻿// 4.2 (4). Длина строк текстового файла не превышает 80 символов.
+// Разработать программу центрирования строк : короткие строки дополняются пробелами и размещаются по центру.
+
+#include "stdafx.h"
+#include "iostream"
+#include "fstream"
+#include "string"
+using namespace std;
+
+ofstream fout("cpp.txt");
+ifstream fin("cpp.txt");
+
+int max_length(char **a, int n)
+{
+	int max_len = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int len = 0;
+		for (int j = 0; a[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		if (len > max_len)max_len = len;
+	}
+	return max_len;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter the number of lines: ";
+	cin >> n;
+
+	char **a = new char*[n];
+	for (int i = 0; i < n; i++)
+		a[i] = new char[80];
+
+	fout << "Before: " << endl << endl;
+	cout << "Enter the lines: ";
+	cin.get();
+	for (int i = 0; i < n; i++)
+	{
+		cin.getline(a[i],80);
+		fout << a[i] << "\n";
+	}
+	int max_len = max_length(a,n);
+	
+	fout << endl << "After: " << endl << endl;
+	for (int i = 0; i < n; i++)
+	{
+		int space;
+		int len = 0;
+		for (int j = 0; a[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		space = (max_len - len) / 2;
+		for (int j = 0; j < space; j++) fout << " ";
+		for (int j = 0; a[i][j] != '\0'; j++)
+		{
+			fout << a[i][j];
+		}
+		fout << endl;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] a[i];
+	}
+	delete[] a;
+
+
+	fin.close(); 
+	fout.close();
+
+
+
+	system("pause");
+	return 0;
+}
+
